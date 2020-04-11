104de1c91b2328e0199d8fbca8f93b471841c4ea
Lucas Lima <lucas.lima@basis.com.br>
2020-04-11 18:03:36 -0300

    Inicia criação dos Controllers

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Domains;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            Console.WriteLine("Get all the Produtos");
+            return _context.Produtos.ToList();
+        }
+    }
+}
